56d7ce227620418ae658e12a61049071ebe59cca
Marylène Henry <marylene.henry@gmail.com>
2025-07-03 11:08:29 +0200

    Fix pipeline

--- R/print_hello_students.R
+++ R/print_hello_students.R
@@ -1,3 +1,12 @@
+#' print_hello_students
+#'
+#' @param x
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' print_hello_students("oui)
 print_hello_students <- function(x){
   print("Hello Students")
 }
--- R/print_hello_world.R
+++ R/print_hello_world.R
@@ -1,3 +1,12 @@
+#' print_hello_world
+#'
+#' @param x
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+#' print_hello_world("oui)
 print_hello_world <- function(x){
   print("Hello World!")
 }
